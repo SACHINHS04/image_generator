252ce59eb431322b3d92ab76f12f2a80e4384478
SACHIN_H_S <46858566+SACHINHS04@users.noreply.github.com>
2023-01-20 12:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import openai_secret_manager
+import openai
 import urllib.request
 
 st.title("Image Generator using OpenAI")
@@ -7,13 +7,17 @@ api_key = st.text_input("Enter your OpenAI API key")
 prompt = st.text_input("Enter a prompt to generate an image")
 
 if st.button("Generate"):
-    # Use the OpenAI API to generate an image
-    response = openai.Image.create(
-        prompt=prompt,
-        api_key=api_key
-    )
-    image_url = response['data']['url']
-    st.image(image_url)
-    file_name = prompt.replace(" ", "_") + ".jpg"
-    urllib.request.urlretrieve(image_url, file_name)
-    st.success("Image saved as {}".format(file_name))
+    if not api_key:
+        st.error("API key is required")
+    else:
+        # Use the OpenAI API to generate an image
+        response = openai.Image.create(
+            prompt=prompt,
+            api_key=api_key
+        )
+        image_url = response['data']['url']
+        st.image(image_url)
+        file_name = prompt.replace(" ", "_") + ".jpg"
+        urllib.request.urlretrieve(image_url, file_name)
+        st.success("Image saved as {}".format(file_name))
+

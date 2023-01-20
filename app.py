2eec8ab5edbe613ddd62dfe7f68add83c10ddfe8
SACHIN_H_S <46858566+SACHINHS04@users.noreply.github.com>
2023-01-20 12:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,19 @@
-import openai
 import streamlit as st
+import openai_secret_manager
+import urllib.request
 
 st.title("Image Generator using OpenAI")
+api_key = st.text_input("Enter your OpenAI API key")
+prompt = st.text_input("Enter a prompt to generate an image")
 
-# Ask the user to input their OpenAI API key
-api_key = st.text_input("Enter your OpenAI API key:")
-openai.api_key = api_key
-
-models = openai.Model.list()
-print(models)
-
-
-def generate_image():
-    prompt = st.text_input("Enter a prompt for the image:")
-    with st.spinner("Waiting for the image to generate..."):
-        completions = openai.Completion.create(
-            engine=model_id,
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-    try:
-        image_url = completions.choices[0].url
-        st.image(image_url, width=300)
-        try:
-            st.markdown("[Download image]({})".format(image_url))
-        except Exception as e:
-            st.error("Error occured, please check the image url or internet connection")
-    except KeyError:
-        st.error("Error occured, please check your prompt or the model you've selected")
-
-if st.button("Generate Image"):
-    generate_image()
+if st.button("Generate"):
+    # Use the OpenAI API to generate an image
+    response = openai.Image.create(
+        prompt=prompt,
+        api_key=api_key
+    )
+    image_url = response['data']['url']
+    st.image(image_url)
+    file_name = prompt.replace(" ", "_") + ".jpg"
+    urllib.request.urlretrieve(image_url, file_name)
+    st.success("Image saved as {}".format(file_name))
